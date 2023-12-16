8c81c1896fe5af3d6ba9565680dcb93781afcda9
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-12-16 10:55:29 -0300

    Removendo classe com nome errado

--- src/main/java/br/com/danieltsuzuki/api/services/exceptions/DataIntegratyViolationException.java
+++
@@ -1,9 +0,0 @@
-package br.com.danieltsuzuki.api.services.exceptions;
-
-public class DataIntegratyViolationException extends RuntimeException{
-
-    public DataIntegratyViolationException(String messasge){
-        super(messasge);
-    }
-
-}
