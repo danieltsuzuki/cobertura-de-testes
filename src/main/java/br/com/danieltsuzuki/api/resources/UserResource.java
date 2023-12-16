74c2ee1fb60cf4d43c0ec4f8359322db43e49e58
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-12-15 23:52:06 -0300

    interface user resource findById

--- src/main/java/br/com/danieltsuzuki/api/resources/UserResource.java
+++ src/main/java/br/com/danieltsuzuki/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.danieltsuzuki.api.resources;
 
 import br.com.danieltsuzuki.api.domain.User;
+import br.com.danieltsuzuki.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(new User(1l, "Daniel", "daniel@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
