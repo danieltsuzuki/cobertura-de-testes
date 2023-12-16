952e77792c45bef2aa1872412333a4ace6ca2e51
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-12-15 23:51:49 -0300

    interface user service impl findById

---
+++ src/main/java/br/com/danieltsuzuki/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.danieltsuzuki.api.services.impl;
+
+import br.com.danieltsuzuki.api.domain.User;
+import br.com.danieltsuzuki.api.repositories.UserRepository;
+import br.com.danieltsuzuki.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
