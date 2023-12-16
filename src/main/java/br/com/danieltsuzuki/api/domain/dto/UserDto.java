83b6576b5d10f833460818664ed349575dba29c1
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-12-16 00:30:02 -0300

    classe user dto

---
+++ src/main/java/br/com/danieltsuzuki/api/domain/dto/UserDto.java
@@ -0,0 +1,27 @@
+package br.com.danieltsuzuki.api.domain.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
