9e42594167946f023dcfae0ebd6cef0ed45db740
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-12-16 02:55:29 -0300

    teste para classe user service metodo findById objeto nao encontrado

--- src/test/java/br/com/danieltsuzuki/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/danieltsuzuki/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.danieltsuzuki.api.services.impl;
 import br.com.danieltsuzuki.api.domain.User;
 import br.com.danieltsuzuki.api.domain.dto.UserDto;
 import br.com.danieltsuzuki.api.repositories.UserRepository;
+import br.com.danieltsuzuki.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -50,6 +51,19 @@ class UserServiceImplTest {
         assertEquals(ID, response.getId());
         assertEquals(NAME, response.getName());
         assertEquals(MAIL, response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyLong())).thenThrow(new ObjectNotFoundException("Objeto nao encontrado"));
+
+        try{
+            service.findById(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto nao encontrado", ex.getMessage());
+        }
+
     }
 
     @Test
